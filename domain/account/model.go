e06d2a71527e2b68b2931b41a0be2749e604a611
matiaspascansky <matias.pascansky@gmail.com>
2024-02-07 17:03:19 -0300

    first models

--- domain/account/model.go
+++ domain/account/model.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+type Status int
+
+const (
+	Overdrawn       Status = 1
+	PositiveBalance Status = 2
+	Inactive        Status = 3
+)
+
+var statusStrings = map[Status]string{
+	Overdrawn:       "Overdrawn",
+	PositiveBalance: "Positive Balance",
+	Inactive:        "Inactive",
+}
+
 type Account struct {
 	Id           string    `json:"id"`
 	DateCreated  time.Time `json:"date_created"`
@@ -15,5 +29,9 @@ type Account struct {
 
 func NewAccountForUser(userId int64) Account {
 	idGenerator := utils.NewAccountIDGenerator()
-	return Account{}
+	return Account{
+		Id:          idGenerator.GenerateID(),
+		DateCreated: time.Now().UTC(),
+		Status:      statusStrings[Inactive],
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module stori-card-challenge-account
+
+go 1.21.6
