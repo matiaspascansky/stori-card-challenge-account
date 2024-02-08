89d7700bd28597f2219f7b2cc621eee56ab3beec
matiaspascansky <matias.pascansky@gmail.com>
2024-02-07 21:50:30 -0300

    request body and validate

--- cmd/web/handler/handler.go
+++ cmd/web/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"stori-card-challenge-account/utils"
@@ -16,8 +17,21 @@ const (
 	aws_config_path = "/var/task/aws_config.json"
 )
 
+type RequestBody struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 // HandleAPIGatewayProxyRequest is the Lambda handler function.
 func HandleAPIGatewayProxyRequest(ctx context.Context, r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var requestBody RequestBody
+	if err := json.Unmarshal([]byte(r.Body), &requestBody); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Wrong type of request",
+		}, nil
+	}
 
 	// Read AWS configuration from JSON file
 	config, err := utils.ReadAWSConfig(aws_config_path)
@@ -29,11 +43,19 @@ func HandleAPIGatewayProxyRequest(ctx context.Context, r events.APIGatewayProxyR
 		}, nil
 	}
 
+	msg := validateRequestModel(requestBody)
+	if msg != "" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       msg,
+		}, nil
+	}
 	// Create an AWS session
 	session, err := session.NewSession(&aws.Config{
 		Region:      aws.String(config.AWSRegion),
 		Credentials: credentials.NewStaticCredentials(os.Getenv("aws_access_key"), os.Getenv("aws_secret_key"), ""),
 	})
+	fmt.Print(session)
 	if err != nil {
 		fmt.Println("Error creating session:", err)
 		return events.APIGatewayProxyResponse{
@@ -41,9 +63,19 @@ func HandleAPIGatewayProxyRequest(ctx context.Context, r events.APIGatewayProxyR
 			Body:       "broken!",
 		}, nil
 	}
-	fmt.Print(session)
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Email has been sent to user!",
+		Body:       "user has been created",
 	}, nil
+}
+
+func validateRequestModel(rb RequestBody) string {
+	if rb.FirstName == "" {
+		return "missing first name"
+	}
+
+	if rb.LastName == "" {
+		return "missing last name"
+	}
+	return ""
 }
